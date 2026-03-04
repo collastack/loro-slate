3038c0504c4a9208c72512a92005fe813817444a
link-duan <itsl@foxmail.com>
2026-03-04 15:05:11 +0800

    Update project configuration and dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
+import dts from "vite-plugin-dts";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), wasm(), topLevelAwait()],
+  plugins: [
+    react(),
+    wasm(),
+    topLevelAwait(),
+    dts({ outDir: "dist", include: ["src"], entryRoot: "src" }),
+  ],
   build: {
     lib: {
       formats: ["es"],
